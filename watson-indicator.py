2b2acbae7ade8686f3d98c5e53ab5558702e3549
Johannes Schweig <johannes.schweig@knime.com>
2018-08-08 09:25:53 +0200

    placed ui updates in functions, stop/restart label is now updated continuously, refactorings

--- watson-indicator.py
+++ watson-indicator.py
@@ -14,16 +14,16 @@ class Indicator():
         self.app = "watson-indicator"
         # icons
         dirname = os.path.dirname(os.path.abspath(__file__))
-        self.icon_passive = os.path.join(dirname, "assets/task_passive.png")
-        self.icon_active = os.path.join(dirname, "assets/task_active.png")
+        self.icon_passive = os.path.join(dirname, "assets/project_passive.png")
+        self.icon_active = os.path.join(dirname, "assets/project_active.png")
         self.indicator = AppIndicator3.Indicator.new(self.app, self.icon_passive, AppIndicator3.IndicatorCategory.OTHER)
         self.indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
         self.indicator.set_menu(self.create_menu())
         # variables
-        self.old_msg = ""
-        self.old_task_active = False # if a task is active
-        self.indicator.set_label(self.old_msg, self.app)
-        self.last_task = "?" # last active task (important for restarting the last task
+        self.old_project = ""
+        self.old_project_active = False # if a project is active
+        self.indicator.set_label(self.old_project, self.app)
+        self.last_project = "?" # last active project (important for restarting the last project)
         # the thread:
         self.update = Thread(target=self.update)
         # daemonize the thread to make the indicator stopable
@@ -31,15 +31,15 @@ class Indicator():
         self.update.start()
 
     def stop_restart(self, source):
-        if self.old_task_active: # if task is running, stop it
+        if self.old_project: # if project is running, stop it
             Popen(["watson", "stop"])
-        else: # if no task is running, restart the last one
+        else: # if no project is running, restart the last one
             Popen(["watson", "restart"])
 
 
     def create_menu(self):
         menu = Gtk.Menu()
-        # menu item "Stop/Restart task"
+        # menu item "Stop/Restart project"
         self.item_stop_restart = Gtk.MenuItem("Loading...")
         self.item_stop_restart.connect('activate', self.stop_restart)
         menu.append(self.item_stop_restart)
@@ -49,43 +49,53 @@ class Indicator():
         menu.show_all()
         return menu
 
+    def update_label(self, project):
+        # check if different project
+        if project != self.old_project:
+            self.old_project = project
+            # change label
+            GObject.idle_add(self.indicator.set_label, project, self.app, priority=GObject.PRIORITY_DEFAULT)
+
+    # update icon
+    def update_icon(self, project_active):
+        # check if different project_active
+        if project_active != self.old_project_active:
+            self.old_project_active = project_active
+            # change icon
+            GObject.idle_add(self.indicator.set_icon, self.icon_active if project_active else self.icon_passive)
+
+    # updates the label of the stop/restart menuitem
+    def update_stop_restart_label(self, project_active):
+        text = ""
+        if project_active: # stop project (time)
+            text = "Stop (" + check_output(["watson", "status", "-e"]).strip() + ")"
+            text = text.replace("minutes ago", "min")
+        else: # restart project
+            text = "Restart '" + self.last_project + "'"
+        # change item label in menu
+        GObject.idle_add(self.item_stop_restart.get_child().set_text, text)
+
+
     def update(self):
         # updates the icon, indicator and menu item labels
         while True:
             time.sleep(1)
-            msg = check_output(["watson", "status"])
-            if "No project started" in msg:
-                msg = ""
-                task_active = False
+            # "project" or "No project started"
+            project = check_output(["watson", "status", "-p"]).strip()
+            if "No project started" in project:
+                project = ""
+                project_active = False
             else:
-                # "Starting project xy at 09:09"
-                start = msg.index("roject") + 7
-                end = msg.index(" ", start)
-                self.last_task = msg[start:end]
-                msg = " " + self.last_task
-                task_active = True
+                self.last_project = project
+                project = " " + self.last_project
+                project_active = True
 
-            # check if different message
-            if msg != self.old_msg:
-                self.old_msg = msg
-                # change label
-                GObject.idle_add(
-                    self.indicator.set_label,
-                    msg, self.app,
-                    priority=GObject.PRIORITY_DEFAULT
-                    )
-            # check if different task_active
-            if task_active != self.old_task_active:
-                self.old_task_active = task_active
-                # change icon
-                GObject.idle_add(
-                    self.indicator.set_icon,
-                    self.icon_active if task_active else self.icon_passive)
-                # change item label in menu
-                text = "Stop" if task_active else ("Restart '" + self.last_task + "'")
-                GObject.idle_add(
-                    self.item_stop_restart.get_child().set_text,
-                    text)
+            # update indicator label
+            self.update_label(project)
+            # update indicator icon
+            self.update_icon(project_active)
+            # update time and project in stop/restart label
+            self.update_stop_restart_label(project_active)
 
 
     def quit(self, source):
